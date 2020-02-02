3e03697de6a626bccf80442b424ca9c92e2fa6c3
Anton Kachurin <katchuring@gmail.com>
2020-02-02 22:04:42 +0300

    Fix recursive typed class handling

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,5 +1,6 @@
+import random
 from dataclasses import dataclass
-from typing import List, Set, Tuple
+from typing import Dict, List, Set, Tuple
 
 from pytest import fixture, mark
 
@@ -10,6 +11,12 @@ from tyaml import load
 class DataClass:
     name: str
     data: dict
+
+
+@dataclass
+class DataClassWithDataClass:
+    id: int
+    items: List[DataClass]
 
 
 @fixture
@@ -27,6 +34,16 @@ def test_typed_list_loader(obj, seq_type):
     assert loaded == [obj]
 
 
+def test_typed_dict_loader(obj):
+    yml = (f"key:\n"
+           f"  name: {obj.name}\n"
+           "  data:\n"
+           f"    a: {obj.data['a']}\n"
+           f"    b: {obj.data['b']}\n")
+    loaded = load(yml, Dict[str, DataClass])
+    assert loaded == {"key": obj}
+
+
 def test_typed_loader(obj):
     yml = (f"name: {obj.name}\n"
            "data:\n"
@@ -34,3 +51,26 @@ def test_typed_loader(obj):
            f"  b: {obj.data['b']}\n")
     loaded = load(yml, DataClass)
     assert loaded == obj
+
+
+@fixture
+def superobj(obj):
+    return DataClassWithDataClass(random.randrange(0xffff), [
+        obj,
+        DataClass("1535", {"a": 1, "b": 3, }),
+    ])
+
+
+def test_nested_typed_loader(superobj):
+    items = superobj.items
+    yml = (f"id: {superobj.id}\n"
+           f"items:\n")
+    yml = yml + "\n".join(
+        f"- name: '{o.name}'\n"
+        f"  data:\n"
+        f"    a: {o.data['a']}\n"
+        f"    b: {o.data['b']}\n"
+        for o in items
+    )
+    loaded = load(yml, DataClassWithDataClass)
+    assert loaded == superobj
--- tyaml/loader.py
+++ tyaml/loader.py
@@ -1,6 +1,8 @@
+from copy import deepcopy
+from dataclasses import is_dataclass
 from typing import Any, Type, Union, _GenericAlias, _SpecialForm
 
-from yaml import FullLoader, MappingNode, SequenceNode
+from yaml import CollectionNode, FullLoader, MappingNode, SequenceNode
 
 from tyaml.types import get_mappings
 
@@ -32,33 +34,30 @@ def _type_tag(typ):
     return f"!!python/object:{typ.__module__}.{typ.__name__}"
 
 
-def _add_single_cls_field_loader(name, typ, loader: Type[FullLoader], base_path: list):
-    base_path = base_path[:]
-    base_path.append((None, name))  # resolve type by key name
-    _add_single_cls_loader(typ, loader, base_path)
-
-
 def _add_single_cls_loader(typ, loader: Type[FullLoader], base_path: list):
     tag = _type_tag(typ)
     field_mappings = get_mappings(typ)
     if not field_mappings:
         return
 
-    loader.add_constructor(tag, lambda l, n: __kwarg_constructor(l, n, typ, field_mappings))
-    loader.add_path_resolver(tag, base_path, MappingNode)
+    loader.add_constructor(tag, lambda l, n: __kwarg_constructor(l, n, typ, field_mappings.copy()))
+    loader.add_path_resolver(tag, base_path[:], MappingNode)
 
-    # if hasattr(typ, "__annotations__"):
-    #     # go deeper
-    #     base_path = base_path[:]
-    #     base_path.append((MappingNode, False))
-    #     for field, typ in typ.__annotations__.items():
-    #         _add_single_cls_field_loader(field, typ, loader, base_path)
+    if hasattr(typ, "__annotations__"):
+        pass
+        # go deeper
+        for field, f_typ in typ.__annotations__.items():
+            if f_typ in [str, int, float]:
+                continue
+            el_path = deepcopy(base_path)
+            el_path.append(field)
+            _add_path_resolvers(f_typ, loader, el_path)
 
 
-def _add_seq_resolver(typ, loader: Type[FullLoader], base_path: list):
+def _add_complex_resolver(typ, loader: Type[FullLoader], base_path: list, node_type: Type[CollectionNode]):
     item_types = [typ]
     base_path = base_path[:]
-    base_path.append((SequenceNode, False))
+    base_path.append((node_type, False))
     if hasattr(typ, "__origin__"):
         if typ.__origin__ is Union:  # Union or Optional
             item_types.extend(typ.__args__)
@@ -67,7 +66,7 @@ def _add_seq_resolver(typ, loader: Type[FullLoader], base_path: list):
         _add_single_cls_loader(typ, loader, base_path)
 
 
-def _add_path_resolvers(typ: TypeOrGeneric, loader: Type[FullLoader], base_path=None):
+def _add_path_resolvers(typ: TypeOrGeneric, loader: Type[FullLoader], base_path: list = None):
     if base_path is None:
         base_path = []
     if _is_generic(typ):
@@ -77,8 +76,11 @@ def _add_path_resolvers(typ: TypeOrGeneric, loader: Type[FullLoader], base_path=
         typ = _real_cls(typ)  # convert typing generics to class
         if typ in [list, tuple, set, frozenset]:
             el_type = args[0]  # type: type
-            _add_seq_resolver(el_type, loader, base_path)
-    else:
+            _add_complex_resolver(el_type, loader, base_path, SequenceNode)
+        if issubclass(typ, dict):
+            el_type = args[1]
+            _add_complex_resolver(el_type, loader, base_path, MappingNode)
+    if is_dataclass(typ) or hasattr(typ, "_fields"):
         _add_single_cls_loader(typ, loader, base_path)
 
 
